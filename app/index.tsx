ddb6fd20c0b1a626dc82c4466193257e67369083
Joseito Fernandes <joseito.terence@gmail.com>
2024-08-01 14:22:36 +0530

    animated logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,14 @@
-import { Text, View } from "react-native";
-import { MotiView } from 'moti';
+import AnimatedLogo from "@/components/AnimatedLogo";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 justify-center items-center"
-    >
-      <MotiView
-        from={{
-          translateX: -12,
-        }}
-        animate={{
-          translateX: 0,
-        }}
-        transition={{
-          type: 'timing',
-          repeat: 5,
-          repeatReverse: true,
-        }}
-      >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-      </MotiView>
+    <View className="flex-1">
+      <View className="
+        absolute top-0 left-0 right-0 bottom-0
+        w-full flex-1 justify-center items-center">
+        <AnimatedLogo />
+      </View>
     </View>
   );
 }
---
+++ components/AnimatedLogo.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { View, Image, Dimensions } from "react-native";
+import { LinearTransition, Easing } from "react-native-reanimated";
+import { MotiView } from 'moti';
+import AppNameTextSVG from "@/components/AppNameSVG";
+
+const SCREEN = Dimensions.get('screen');
+
+const AnimatedLogo = () => {
+  const [showText, setShowText] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowText(true)
+    }, 1000)
+  }, [])
+
+  return (
+    <MotiView 
+      className="flex-row"
+      layout={LinearTransition.easing(Easing.ease).duration(500)}
+      from={{
+        translateX: 0,
+        translateY: 0,
+        scale: 1,
+      }}
+      animate={{
+        translateX: (SCREEN.width - 200) / 2,
+        translateY: -(SCREEN.height - 200) / 2,
+        scale: 0.4,
+      }}
+      transition={{
+        type: 'timing',
+        duration: 1000,
+        delay: 2000,
+      }}
+    >
+      <Image 
+        source={require('../assets/images/adaptive-icon.png')}
+        className="w-[195px] h-[195px]"
+      />
+      <View className="overflow-hidden translate-x-[-40px]">
+        {showText && 
+          <MotiView
+            from={{
+              opacity: 0,
+              translateX: -SCREEN.width,
+            }}
+            animate={{
+              opacity: 1,
+              translateX: 0,
+            }}
+            transition={{
+              type: 'timing',
+              duration: 1000,
+            }}
+            className="z-[-1]"
+          >
+            <AppNameTextSVG />
+          </MotiView>
+        }
+      </View>
+    </MotiView>
+  )
+}
+
+export default AnimatedLogo;
