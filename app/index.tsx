317803d873dd819fd0598bb58250789a29a4c465
Joseito Fernandes <joseito.terence@gmail.com>
2024-08-03 23:31:33 +0530

    feat: Add MotiImage component for displaying keyboard image glow effect colors adjusted

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import AnimatedLogo from "@/components/AnimatedLogo";
 import { View } from "react-native";
 import { BlurView } from 'expo-blur';
-import { MotiView } from "moti";
+import { MotiView, MotiImage } from "moti";
 import { LinearGradient } from "expo-linear-gradient";
 import Circle from "@/components/Circle";
 
@@ -22,22 +22,34 @@ export default function Index() {
         transition={{ type: 'timing', duration: 1000, delay: 2000 }}
         className="w-full flex-1"
       >
-        <View className="items-center flex-row py-20 overflow-hidden">
+        <View className="items-cente flex-row py-20 overflow-hidden">
           <MotiView 
             from={{ translateY: -20, translateX: 10, scale: 0.9 }}
             animate={{ translateY: 0, translateX: 0, scale: 1 }}
             transition={{ type: 'timing', duration: 3000, loop: true }}
-            className="-ml-[200px]"
+            className="-ml-[200px] -mt-[100px]"
           >
-            <Circle size={450} colors={['#fffe00 0%', '#faff59 40%', '#fffed2 100%']} />
+            <MotiView
+              from={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ type: 'timing', duration: 1000, delay: 1800 }}
+            >
+              <Circle size={500} colors={['#1dc9de 0%', '#0a3b41 100%']} />
+            </MotiView>
           </MotiView>
           <MotiView 
-            from={{ translateY: -20, translateX: -20, scale: 0.9 }}
+            from={{ translateY: -20, translateX: -20, scale: 0.8 }}
             animate={{ translateY: 20, translateX: 0, scale: 1 }}
             transition={{ type: 'timing', delay: 2000, duration: 3000, loop: true }}
-            className="-ml-[20px]"
+            className="-ml-[100px] -mt-[80px]"
           >
-            <Circle size={450} colors={['#ff0000 0%', '#ff5959 90%', '#ffd2d2 100%']} />
+            <MotiView
+              from={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ type: 'timing', duration: 1000, delay: 1600 }}
+            >
+              <Circle size={450} colors={['#41a9e3 10%', '#183f54 100%']} />
+            </MotiView>
           </MotiView>
           <BlurView 
             intensity={80}
@@ -50,6 +62,16 @@ export default function Index() {
             end={[0.5, 1]}
             style={{ height: 100, width: '100%', position: 'absolute', bottom: 0 }}
           />
+          <View className="absolute left-0 right-0 -bottom-16">
+            <MotiImage 
+              source={require("../assets/images/keyboard.png")}
+              className="w-[350px] mx-auto"
+              resizeMode="contain"
+              from={{ translateY: 0, opacity: 0, scale: 0.5 }}
+              animate={{ translateY: 50, opacity: 1, scale: 1 }}
+              transition={{ type: 'timing', duration: 1000, delay: 2000 }}
+            />
+          </View>
         </View>
       </MotiView>
     </View>
