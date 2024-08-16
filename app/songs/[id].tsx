d731a7110aae11ceaa4f3f946c0ee1a179936b19
Joseito Fernandes <joseito.terence@gmail.com>
2024-08-17 00:14:28 +0530

    wip: bottom sheet for single song screen

--- app/songs/[id].tsx
+++ app/songs/[id].tsx
@@ -6,6 +6,8 @@ import { Button } from '@/components/Button'
 import { supabase } from '@/utils/supabase'
 import { AntDesign } from '@expo/vector-icons'
 import { useQuery } from '@tanstack/react-query'
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 const CARD_SIZE = 300
 
@@ -39,7 +41,7 @@ export default function Song() {
       <View className='flex-1 items-center justify-center'>
         <View
           className='bg-gray-800 rounded-[20px] overflow-hidden'
-          style={{ width: CARD_SIZE, height: CARD_SIZE }}
+          style={{ width: CARD_SIZE, height: CARD_SIZE, marginBottom: 150 }}
         >
           <Image
             source={{ uri: song?.cover_image! }}
@@ -49,6 +51,46 @@ export default function Song() {
             resizeMode='cover'
           />
         </View>
+        <BottomSheet
+          snapPoints={[150, 300, '100%']}
+        >
+          <BottomSheetScrollView>
+            <View className='p-6'>
+              <View className='flex-row items-center justify-between'
+                style={{ display: song?.style ? 'flex' : 'none' }}
+              >
+                <Text className='text-black text-lg font-semibold'>
+                  {song?.style}
+                </Text>
+
+                {/* {(song?.tempo && song?.tempo > 0) && ( */}
+                  <View className='flex-row items-center'
+                    style={{ display: song?.tempo && song?.tempo > 0 ? 'flex' : 'none' }}
+                  >
+                    <MaterialCommunityIcons name="music-note-quarter" size={22} color="black" />
+                    <Text className='text-black text-lg font-semibold'>
+                      = {song?.tempo} {typeof song?.tempo}
+                    </Text>
+                  </View>
+                {/* )} */}
+              </View>
+
+              <Text className='text-black text-3xl font-bold capitalize'>
+                {song?.title}
+              </Text>
+
+              <View className='mt-10'>
+                <Text className='text-black text-lg font-semibold'>
+                  Lyrics
+                </Text>
+                <Text className='text-black text-sm font-semibold'>
+                  {song?.lyrics}
+                </Text>
+              </View>
+
+            </View>
+          </BottomSheetScrollView>
+        </BottomSheet>
       </View>
     </SafeAreaView>
   )
