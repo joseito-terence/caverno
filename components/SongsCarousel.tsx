768a372acc1fde3b39115b1ffea814984a1f0c68
Joseito Fernandes <joseito.terence@gmail.com>
2024-08-22 22:33:19 +0530

    refactor: Update SongsCarousel to use react-native-reanimated-carousel

--- components/SongsCarousel.tsx
+++ components/SongsCarousel.tsx
@@ -1,12 +1,14 @@
+import { useEffect } from 'react';
 import { Dimensions, View, Image, Text, TouchableOpacity } from 'react-native'
-import Animated, { Extrapolation, interpolate, SharedValue, useAnimatedRef, useAnimatedStyle, useScrollViewOffset } from 'react-native-reanimated'
 import Entypo from '@expo/vector-icons/Entypo';
 import { Database } from '@/utils/database.types';
 import { router } from 'expo-router';
 import { useQueryClient } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import Carousel from "react-native-reanimated-carousel";
 
 const SCREEN = Dimensions.get('screen')
+const SONG_CARD_WIDTH = SCREEN.width * 0.8
+const SONG_CARD_HEIGHT = SONG_CARD_WIDTH * 1.3
 
 type TSong = Database['public']['Tables']['songs']['Row']
 
@@ -17,130 +19,83 @@ interface Props {
 const SongsCarousel = ({
   songs = []
 }: Props) => {
-  const scrollViewRef = useAnimatedRef<Animated.ScrollView>()
-  const scrollOffset = useScrollViewOffset(scrollViewRef)
-  const ListPadding = SCREEN.width - SONG_CARD_WIDTH
-
   return (
-    <View
-      className='width-full'
-      style={{
-        height: SONG_CARD_HEIGHT,
-      }}
-    >
-      <Animated.ScrollView
-        ref={scrollViewRef}
-        horizontal
-        scrollEventThrottle={16}
-        snapToInterval={SONG_CARD_WIDTH}
-        decelerationRate='fast'
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{
-          width: SONG_CARD_WIDTH * songs.length + ListPadding,
+    <View className='w-full flex-1 justify-center items-center'>
+      <Carousel
+        style={{
+          width: SCREEN.width,
+          height: SONG_CARD_HEIGHT,
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
         }}
-      >
-        {songs.map((song, index) => (
-          <SongCard
-            key={index}
-            index={index}
-            song={song}
-            scrollOffset={scrollOffset}
-          />
-        ))}
-      </Animated.ScrollView>
+        width={SONG_CARD_WIDTH}
+        height={SONG_CARD_HEIGHT}
+        pagingEnabled
+        snapEnabled
+        mode='horizontal-stack'
+        loop={false}
+        data={songs}
+        modeConfig={{
+          snapDirection: 'left',
+          stackInterval: 18,
+        }}
+        customConfig={() => ({ type: "positive", viewCount: 0 })}
+        renderItem={renderItem}
+      />
     </View>
   )
 }
 
+const renderItem = ({ item }: any) => <SongCard song={item} />
+
 export default SongsCarousel;
 
-const SONG_CARD_WIDTH = SCREEN.width * 0.8
-const SONG_CARD_HEIGHT = SONG_CARD_WIDTH * 1.3
-
-export interface SongCardProps {
-  index: number;
-  song: TSong;
-  scrollOffset: SharedValue<number>;
-}
-
-export const SongCard = ({
-  index,
-  song,
-  scrollOffset
-}: SongCardProps) => {
+const SongCard = ({
+  song
+}: {
+  song: TSong
+}) => {
   const queryClient = useQueryClient()
-  const rContainerStyle = useAnimatedStyle(() => {
-    const activeIndex = scrollOffset.value / SONG_CARD_WIDTH
-    const paddingLeft = (SCREEN.width - SONG_CARD_WIDTH) / 3
-
-    const translateX = interpolate(
-      activeIndex,
-      [index - 2, index - 1, index, index + 1],
-      [120, 60, 0, -SONG_CARD_WIDTH - paddingLeft * 2],
-      Extrapolation.CLAMP
-    )
-
-    const scale = interpolate(
-      activeIndex,
-      [index - 2, index - 1, index, index + 1],
-      [0.8, 0.9, 1, 1],
-      Extrapolation.CLAMP
-    )
-
-    return {
-      left: paddingLeft,
-      transform: [
-        { translateX: scrollOffset.value + translateX },
-        { scale },
-      ]
-    }
-  }, [])
 
   useEffect(() => {
     queryClient.setQueryData(['songs', song.id], song)
   }, [])
 
   return (
-    <Animated.View
-      style={[
-        {
-          zIndex: 10 - index,
-          backgroundColor: 'black',
-        },
-        rContainerStyle
-      ]}
+    <TouchableOpacity
+      activeOpacity={1}
+      onPress={() => router.push(`/songs/${song.id}`)}
+      style={{
+        width: SONG_CARD_WIDTH,
+        height: SONG_CARD_HEIGHT,
+      }}
     >
-      <TouchableOpacity
-        activeOpacity={1}
-        onPress={() => router.push(`/songs/${song.id}`)}
-        className='h-full'
+      <Image
+        source={{ uri: song.cover_image! }}
+        width={SONG_CARD_WIDTH}
+        height={SONG_CARD_HEIGHT}
+        className='rounded-[20px] absolute'
+        resizeMode='cover'
+      />
+
+      <View
+        className='
+        absolute bottom-0 rounded-[20px] bg-black h-20 px-8
+        flex-row justify-between items-center
+      '
+        style={{
+          width: SONG_CARD_WIDTH,
+        }}
       >
-        <Image
-          source={{ uri: song.cover_image! }}
-          width={SONG_CARD_WIDTH}
-          height={SONG_CARD_HEIGHT}
-          className='rounded-[20px] absolute'
-          resizeMode='cover'
-        />
+        <Text numberOfLines={2} className='text-white text-lg font-semibold flex-1'>
+          {song.title}
+        </Text>
 
-        <View
-          className='
-          absolute bottom-0 rounded-[20px] bg-black h-20 px-8
-          flex-row justify-between items-center
-        '
-          style={{
-            width: SONG_CARD_WIDTH,
-          }}
-        >
-          <Text numberOfLines={2} className='text-white text-lg font-semibold flex-1'>
-            {song.title}
-          </Text>
-
-          <View className='w-12 h-12 rounded-full bg-gray-800/95 justify-center items-center'>
-            <Entypo name="controller-play" size={24} color="white" />
-          </View>
+        <View className='w-12 h-12 rounded-full bg-gray-800/95 justify-center items-center'>
+          <Entypo name="controller-play" size={24} color="white" />
         </View>
-      </TouchableOpacity>
-    </Animated.View>
+      </View>
+    </TouchableOpacity>
   )
 }
