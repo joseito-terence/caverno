a90f1bb8de4fe1658713f2b60d4a70193be008b5
Joseito Fernandes <joseito.terence@gmail.com>
2025-04-26 23:22:24 +0100

    refactor: enhance back navigation handling in SongsBottomSheet
    - Integrated BackHandler to manage hardware back press events, allowing the bottom sheet to collapse to its initial state when not at the top index.
    - Utilized useRef to maintain a reference to the BottomSheet component for improved control over its state.
    - Updated the onChange prop to track the current index of the bottom sheet for better navigation experience.

--- components/SongsBottomSheet.tsx
+++ components/SongsBottomSheet.tsx
@@ -1,19 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
+import { BackHandler } from 'react-native';
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
-import SongsList from "./SongsList";
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import SongsList from "./SongsList";
 
 const SNAP_POINTS = [240, 600, '88%']
 
 export default function SongsBottomSheet() {
   const insets = useSafeAreaInsets()
+  const ref = useRef<BottomSheet>(null)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const backAction = () => {
+      if (
+        ref.current &&
+        currentIndex !== 0
+      ) {
+        ref.current.snapToIndex(0);
+        return true;
+      }
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => backHandler.remove();
+  }, [currentIndex])
 
   return (
     <BottomSheet
+      ref={ref}
       snapPoints={SNAP_POINTS}
       backgroundStyle={{ backgroundColor: 'black' }}
       containerStyle={{ zIndex: 100 }}
       enableDynamicSizing={false}
       topInset={insets.top}
+      onChange={setCurrentIndex}
     >
       <BottomSheetView className="flex-1">
         <SongsList />
