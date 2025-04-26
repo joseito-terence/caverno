4292510eae073a1f6a41d190f769b356bdf35914
Joseito Fernandes <joseito.terence@gmail.com>
2025-04-26 23:10:19 +0100

    refactor: update snap points in SongsBottomSheet for improved layout
    - Adjusted the snap points in SongsBottomSheet from 200 to 240 for better control over the bottom sheet's height.

--- components/SongsBottomSheet.tsx
+++ components/SongsBottomSheet.tsx
@@ -2,7 +2,7 @@ import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import SongsList from "./SongsList";
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-const SNAP_POINTS = [200, 600, '88%']
+const SNAP_POINTS = [240, 600, '88%']
 
 export default function SongsBottomSheet() {
   const insets = useSafeAreaInsets()
