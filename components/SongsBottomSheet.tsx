bd023e0c9e4e6691c1c6009dafcae9f80fb076b7
Joseito Fernandes <joseito.terence@gmail.com>
2025-04-27 09:52:51 +0100

    refactor: replace useEffect with useFocusEffect in SongsBottomSheet for improved back navigation handling
    - Switched from useEffect to useFocusEffect to better manage back navigation when the bottom sheet is focused.
    - Removed the dependency array from the effect to ensure the back action is always registered when the component is focused.

--- components/SongsBottomSheet.tsx
+++ components/SongsBottomSheet.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { BackHandler } from 'react-native';
 import BottomSheet from "@gorhom/bottom-sheet";
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import SongsList from "./SongsList";
+import { useFocusEffect } from 'expo-router';
 
 const SNAP_POINTS = [240, 600, '88%']
 
@@ -11,7 +12,7 @@ export default function SongsBottomSheet() {
   const ref = useRef<BottomSheet>(null)
   const [currentIndex, setCurrentIndex] = useState(0)
 
-  useEffect(() => {
+  useFocusEffect(() => {
     const backAction = () => {
       if (
         ref.current &&
@@ -26,7 +27,7 @@ export default function SongsBottomSheet() {
     const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
 
     return () => backHandler.remove();
-  }, [currentIndex])
+  })
 
   return (
     <BottomSheet
